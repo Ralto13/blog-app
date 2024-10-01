91c45cc14df1b7bc856bc204ccad93f23dd11ef6
Ralto13 <doorumi13@gmail.com>
2024-10-01 22:27:40 +0900

    FirebaseStore 'create'

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,58 +1,86 @@
-import { useState } from "react";
+import AuthContext from "context/AuthContext";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 interface PostsListProps {
-  hasNavigation?: boolean
+  hasNavigation?: boolean;
 }
 
 type TabType = "all" | "my";
 
+export interface PostProps {
+  id?: string;
+  title: string;
+  email: string;
+  summary: string;
+  content: string;
+  createdAt: string;
+}
+
 export default function PostList({ hasNavigation = true }: PostsListProps) {
   const [activeTab, setActiveTab] = useState<TabType>("all");
+  const [posts, setPosts] = useState<any>([]);
+  const { user } = useContext(AuthContext);
+
+  const getPosts = async () => {
+    const datas = await getDocs(collection(db, "posts"));
+
+    datas?.forEach((doc) => {
+      const dataObj = { ...doc.data(), id: doc.id };
+      setPosts((prev: any) => [...prev, dataObj as PostProps]);
+    });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <>
       {hasNavigation && (
         <div className="post__navigation">
-          <div 
-          role="presentation"
-          onClick={()=> setActiveTab("all")}
-          className={activeTab === "all" ? "post__navigation--active" : ""}>전체</div>
-          <div 
-          role="presentation"
-          onClick={()=> setActiveTab("my")}
-          className={activeTab === "my" ? "post__navigation--active" : ""}>나의 글</div>
+          <div
+            role="presentation"
+            onClick={() => setActiveTab("all")}
+            className={activeTab === "all" ? "post__navigation--active" : ""}
+          >
+            전체
+          </div>
+          <div
+            role="presentation"
+            onClick={() => setActiveTab("my")}
+            className={activeTab === "my" ? "post__navigation--active" : ""}
+          >
+            나의 글
+          </div>
         </div>
       )}
       <div className="post__list">
-        {[...Array(10)].map((e, index) => (
-          <div key={index} className="post__box">
-            <Link to={`/posts/${index}`}>
-              <div className="post__profile-box">
-                <div className="post__profile" />
-                <div className="post__author-name">나</div>
-                <div className="post__date">2024-09-07 일요일</div>
+        {posts?.length > 0
+          ? posts?.map((post: any) => (
+              <div key={post?.id} className="post__box">
+                <Link to={`/posts/${post?.id}`}>
+                  <div className="post__profile-box">
+                    <div className="post__profile" />
+                    <div className="post__author-name">{post?.email}</div>
+                    <div className="post__date">{post?.createdAt}</div>
+                  </div>
+                  <div className="post__title">{post?.title}</div>
+                  <div className="post__text">{post?.summary}</div>
+                </Link>
+                {post?.email === user?.email && (
+                  <div className="post__utils-box">
+                    <div className="post__delete">삭제</div>
+                    <Link to={`/posts/edit/${post?.id}`} className="post__edit">
+                      수정
+                    </Link>
+                  </div>
+                )}
               </div>
-              <div className="post__title">게시글 {index}</div>
-              <div className="post__text">
-                orem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </div>
-              <div className="post__utils-box">
-                <div className="post__delete">삭제</div>
-                <div className="post__edit">수정</div>
-              </div>
-            </Link>
-          </div>
-        ))}
+            ))
+          : <div className="post__no-post">게시글이 없습니다.</div>}
       </div>
     </>
   );
--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,5 +1,7 @@
 import { initializeApp, FirebaseApp, getApp } from "firebase/app";
-import "firebase/auth"
+import "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 export let app: FirebaseApp;
 
@@ -20,5 +22,7 @@ try {
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
 
 export default firebase;

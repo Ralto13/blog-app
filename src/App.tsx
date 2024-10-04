8dd7fb529271f1fac000389cd2a368ac47724bcd
Ralto13 <doorumi13@gmail.com>
2024-10-04 23:00:24 +0900

    add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { app, db } from "firebaseApp";
+import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useState,useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
+import "react-toastify/dist/ReactToastify.css";
+import ThemeContext from "context/ThemeContext";
 
 import Router from "./components/Router";
 import Loader from "components/Loader";
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
   // auth를 체크하기전 (initialize)에는 loader를 띄워주는 용도
   const [init, setInit] = useState<boolean>(false);
@@ -24,15 +25,15 @@ function App() {
       } else {
         setIsAuthenticated(false);
       }
-      setInit(true)
+      setInit(true);
     });
-  },[auth])
+  }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === "light" ? "white" : "dark"}>
       <ToastContainer />
-      { init ? <Router isAuthenticated={isAuthenticated} /> : <Loader /> }
-    </>
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
+    </div>
   );
 }
 
